3b614b265829d5a173b8a85b02a788eda007b495
Julián García Pallas <juliangarcia940@gmail.com>
2023-09-15 00:41:40 +0200

    Add configuration to support h2 console

--- src/main/java/es/udc/fi/dc/fd/rest/common/SecurityConfig.java
+++ src/main/java/es/udc/fi/dc/fd/rest/common/SecurityConfig.java
@@ -3,6 +3,7 @@ package es.udc.fi.dc.fd.rest.common;
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -15,6 +16,8 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import jakarta.servlet.SessionCookieConfig;
 
 /**
  * The Class SecurityConfig.
@@ -47,12 +50,26 @@ public class SecurityConfig {
                 .requestMatchers(antMatcher("/api/users/signUp")).permitAll()
                 .requestMatchers(antMatcher("/api/users/login")).permitAll()
                 .requestMatchers(antMatcher("/api/users/loginFromServiceToken")).permitAll()
+                .requestMatchers(antMatcher("/h2-console/**")).permitAll()
                 .anyRequest().authenticated()
             )
             .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
+            http.csrf().disable();
+            http.headers().frameOptions().disable();
         // @formatter:on
 
         return http.build();
+    }
+
+    @Bean
+    public ServletContextInitializer servletContextInitializer() {
+        return servletContext -> {
+            // Enable SameSite=None for all cookies
+            SessionCookieConfig sessionCookieConfig = servletContext.getSessionCookieConfig();
+            sessionCookieConfig.setHttpOnly(true);
+            sessionCookieConfig.setSecure(true); // Only set this to true if using HTTPS
+        };
     }
 
     /**
